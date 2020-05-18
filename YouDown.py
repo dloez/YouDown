e1e1495c57c657c72cff7b54acecd8c20d81e0de
David <davidlopez.hellin@outlook.com>
2020-05-18 17:02:59 +0200

    Now it detects videos in clipboard

--- YouDown.py
+++ YouDown.py
@@ -1,7 +1,23 @@
+import time
+import win32clipboard
 from pytube import YouTube
 
+last_url = ""
 while True:
-    url = input("Introduce la url del video que quieres descargar: ")
-    YouTube(url).streams.get_highest_resolution().download()
-    print("Video descargado")
-    print("#########################################################")
+    try:
+        win32clipboard.OpenClipboard()
+        url = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+    except:
+        url = ""
+
+    if url and last_url != url:
+        if url.startswith("https://www.youtube.com/watch?v"):
+            print("Video detectado, descargando...")
+            YouTube(url).streams.get_highest_resolution().download()
+            print("Video descargado")
+            print("#########################################################")
+
+            last_url = url
+
+    time.sleep(0.1)
