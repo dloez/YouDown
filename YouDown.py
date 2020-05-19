abb1966fd80fe45cb964e8a5745e78a9e045bb1a
David <davidlopez.hellin@outlook.com>
2020-05-19 13:42:12 +0200

    Fixed clipboard without content and added output folder for videos

--- YouDown.py
+++ YouDown.py
@@ -1,20 +1,35 @@
 import time
 import win32clipboard
 from pytube import YouTube
+import os
+import json
 
+
+LAST_URL_FILE = "last_url.json"
+OUTPUT_FOLDER = "videos"
+
+
+def init_clipboard():
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText("Launched YouDown!")
+    win32clipboard.CloseClipboard()
+
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.mkdir(OUTPUT_FOLDER)
+
+
+init_clipboard()
 last_url = ""
 while True:
-    try:
-        win32clipboard.OpenClipboard()
-        url = win32clipboard.GetClipboardData()
-        win32clipboard.CloseClipboard()
-    except:
-        url = ""
+    win32clipboard.OpenClipboard()
+    url = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
 
     if url and last_url != url:
         if url.startswith("https://www.youtube.com/watch?v"):
             print("Video detectado, descargando...")
-            YouTube(url).streams.get_highest_resolution().download()
+            YouTube(url).streams.get_highest_resolution().download(OUTPUT_FOLDER)
             print("Video descargado")
             print("#########################################################")
 
